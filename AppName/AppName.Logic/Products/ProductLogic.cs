e78c4abde7f418dc3c4b396f794634337160d314
gradampl <gr_adam@o2.pl>
2019-03-04 19:48:26 +0100

    Further development...

--- AppName/AppName.Logic/Products/ProductLogic.cs
+++ AppName/AppName.Logic/Products/ProductLogic.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AppName.Logic.Interfaces;
+using AppName.Logic.Repositories;
+using AppName.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,37 @@ using System.Threading.Tasks;
 
 namespace AppName.Logic.Products
 {
-    class ProductLogic
+    public class ProductLogic : IProductLogic
     {
+        private IProductRepository _repository;
+
+        public ProductLogic(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Result<Product> Create(Product product)
+        {
+            if (product == null)
+            {
+                throw new ArgumentNullException(nameof(product));
+            }
+
+            //walidacja
+            _repository.Addd(product);
+            _repository.SaveChanges();
+
+            return Result.Ok(product);
+        }
+
+        public Result<IEnumerable<Product>> GetAllActive()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Result<Product> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
